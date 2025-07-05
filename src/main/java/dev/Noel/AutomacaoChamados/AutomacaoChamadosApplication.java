8bc92876c8b82e426b474a539ddaf3629475beac
WagnerGrave <wagner.grave@gmail.com>
2025-07-05 12:13:13 -0300

    first commit

---
+++ src/main/java/dev/Noel/AutomacaoChamados/AutomacaoChamadosApplication.java
@@ -0,0 +1,13 @@
+package dev.Noel.AutomacaoChamados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomacaoChamadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomacaoChamadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/Noel/AutomacaoChamados/AutomacaoChamadosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Noel.AutomacaoChamados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomacaoChamadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
